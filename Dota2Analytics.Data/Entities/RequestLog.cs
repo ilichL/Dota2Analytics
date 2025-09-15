13345989e0dfef6100d571099b1e132a90347ed4
ilichL <ilich.02@yandex.ru>
2025-09-15 17:01:49 +0300

    addRequestLog

--- Dota2Analytics.Data/Entities/RequestLog.cs
+++ Dota2Analytics.Data/Entities/RequestLog.cs
@@ -1,9 +1,4 @@
 ﻿using Dota2Analytics.Data.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dota2Analytics.Data.Entities
 {
